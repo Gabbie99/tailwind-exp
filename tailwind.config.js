34622e0dc26f27e8790698ca8980d1b27e6604f6
Gabbie99 <khoinom2018@gmail.com>
2022-10-25 17:31:30 +0100

    still working

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,25 @@ module.exports = {
   theme: {
     extend: {
       brightness:{
-        "brightness":"20"
+        "brightness":"8"
       },
       backgroundImage:{
-        "perfect-suit":"url('/img/perfect suit.jpg')"
+        "mini":"url('/img/mini.jpg')",
+        "poster":"url('/img/mini2.jpg')" ,
+
       },
       screens:{
         "sm":"480"
       },
       spacing:{
         "big":"48rem"
+      },
+      backgroundColor:{
+       "linear": "linear-gradient(146deg, rgba(45,58,87,1) 19%, rgba(45,58,87,1) 30%, rgba(45,58,87,1) 44%, rgba(24,39,71,1) 45%, rgba(255,255,255,1) 45%)"
       }
     },
     fontFamily:{
-      nunito:['Nunito', 'sans-serif']
+      bioRhyme:['BioRhyme', 'serif']
     }
   },
   plugins: [],
